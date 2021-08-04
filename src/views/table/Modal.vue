8bf6c0cbf61ba8c8fb738ce12ddf44acceb93e3e
Teewhy <toyosiazuh.at@gmail.com>
2021-08-04 15:33:28 +0100

    add table view for products

---
+++ src/views/table/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Kindly fill this</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal">
+                        <div class="form-group text-left">
+                            <label for="">Product Name</label>
+                            <input type="text" class="form-control" placeholder="Product Name"/>
+                        </div>
+
+                        <div class="form-group text-left">
+                            <label for="">Price</label>
+                            <input type="text" class="form-control" placeholder="Price"/>
+                        </div>
+
+                        <div class="form-group text-left">
+                            <label for="">Location</label>
+                            <input type="text" class="form-control" placeholder="Location"/>
+                        </div>
+
+                        <div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "Modal",
+    }
+</script>
+
+<style scoped>
+    label{
+        font-weight: bold;
+    }
+</style>
---
+++ src/views/table/Table.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container"> 
+        <div class="row">
+            <div class="col-lg-12 d-flex">
+                <div class="mr-auto"> 
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+                <div class="w-25"> 
+                    <button class="btn btn-primary w-100" type="button" data-toggle="modal" data-target="#exampleModal">Add</button>
+                    <Modal/>
+                </div>
+            </div>
+        </div>
+        <table class="table table-striped table-dark mt-4">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import Modal from './Modal.vue'
+    export default {
+        name: "Table",
+        components: {
+            Modal
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
